2ed54184fd935c4a7ca1ac2cb49b0e99ae7a5676
CCMPacheco <pachecoccmp@gmail.com>
2022-04-08 13:02:54 -0300

    Test 04 pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    let result = [...array];
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < args.length; j++) {
+            if(array[i] === args[j]) {
+                result.splice(i, 1, '');
+            }
+        }
+    }
+    return result.filter(n => n);
 };
 
 // Do not edit below this line
