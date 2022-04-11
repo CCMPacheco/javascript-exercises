f3b5fb29201308b3c49b26ed6d0652dfd51782ba
CCMPacheco <pachecoccmp@gmail.com>
2022-04-11 18:47:34 -0300

    Test 10 pass

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  if (number < 0) {
+    return "OOPS";
+  }
+  let fibonacci = [];
+  fibonacci[0] = 0;
+  fibonacci[1] = 1;
 
+  for (let i = 2; i <= number; i++) {
+    fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+  }
+  return fibonacci[number];
 };
 
 // Do not edit below this line
