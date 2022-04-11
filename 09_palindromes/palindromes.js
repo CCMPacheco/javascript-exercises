035a0fb2b746c8073875d102f555d74b52422358
CCMPacheco <pachecoccmp@gmail.com>
2022-04-10 23:47:10 -0300

    Test 09 pass.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let normalized = string
+    .toLowerCase()
+    .replace(/[^a-z]+/g, "")
+    .split("");
 
+  let reversed = [...normalized].reverse();
+
+  for (let i = 0; i < normalized.length; i++) {
+    if (normalized[i] !== reversed[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
