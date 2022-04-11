768a3a91d5c42674f9b146f27c218c6a62ffba60
CCMPacheco <pachecoccmp@gmail.com>
2022-04-11 19:35:13 -0300

    Test 12 pass

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (person) {
+  const oldest = person.sort((a, b) => {
+    if (a.yearOfDeath && b.yearOfDeath) {
+      const lastPerson = a.yearOfDeath - a.yearOfBirth;
+      const nextPerson = b.yearOfDeath - b.yearOfBirth;
+      return lastPerson > nextPerson ? -1 : 1;
+    } else if (!a.yearOfDeath) {
+      const currentYear = new Date().getFullYear();
+      const lastPerson = currentYear - a.yearOfBirth;
+      const nextPerson = b.yearOfDeath - b.yearOfBirth;
+      return lastPerson > nextPerson ? -1 : 1;
+    } else if (!b.yearOfDeath) {
+      const currentYear = new Date().getFullYear();
+      const lastPerson = a.yearOfDeath - a.yearOfBirth;
+      const nextPerson = currentYear - b.yearOfBirth;
+      return lastPerson > nextPerson ? -1 : 1;
+    }
+  });
+  return oldest[0];
 };
 
 // Do not edit below this line
